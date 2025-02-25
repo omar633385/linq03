f231a7cdb468676edb249e97239095a4da2dd59b
3omar63 <om728526@gmail.com>
2025-02-25 11:26:55 +0200

    Add project files.

---
+++ linq03/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using static linq03.ListGenerator;
+namespace linq03
+{
+    internal class Program
+    {
+        static void PrintCollection(IEnumerable collection)
+        {
+            foreach (var item in collection)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static void Main(string[] args)
+        {
+            #region SetOperators
+
+            #region 1. Find the unique Category names from Product List
+
+            var UniqueCategories= ProductsList.Select(p => p.Category).Distinct();
+            PrintCollection(UniqueCategories);
+            #endregion
+            #endregion
+        }
+    }
+}
